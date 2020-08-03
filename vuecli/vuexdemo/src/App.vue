ad8151123ac6a13b964ee9b766dc5ddfdc76a2e5
sunc <285594594@qq.com>
2020-08-03 13:28:24 +0800

    webpack例子

---
+++ vuecli/vuexdemo/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <h2 >{{$store.state.counter}}</h2>
+    <button @click="btnAdd">add</button>
+    <button @click="btnSub">sub</button>
+    <h-w></h-w>
+  </div>
+</template>
+
+<script>
+  import HW from '@/components/HelloWorld'
+  export default {
+    name: 'App',
+    data() {
+      return {
+        msg: '11111',
+        counter: 111
+      }
+    },
+    methods:{
+      btnAdd(){
+        this.$store.commit('increment')
+      },
+      btnSub(){
+        this.$store.commit('decrement')
+      }
+    },
+    components:{
+      HW
+    }
+  }
+</script>
+
+<style>
+</style>
